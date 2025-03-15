52c6b546aefef7e9a25408ab8109ae66026f3de1
lota <christiansolomon592@gmail.com>
2025-03-15 23:24:57 +0100

    deepseek integration

---
+++ src/test/java/com/lota/ai/deepseek_integration/DeepseekIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lota.ai.deepseek_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepseekIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
