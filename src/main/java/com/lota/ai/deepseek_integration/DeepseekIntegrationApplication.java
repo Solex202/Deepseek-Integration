767978b9835083ae4f6b5dd086195334c5f27a53
lota <christiansolomon592@gmail.com>
2025-03-18 12:07:06 +0100

    deepseek integration

--- src/main/java/com/lota/ai/deepseek_integration/DeepseekIntegrationApplication.java
+++ src/main/java/com/lota/ai/deepseek_integration/DeepseekIntegrationApplication.java
@@ -3,9 +3,6 @@ package com.lota.ai.deepseek_integration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 @SpringBootApplication
 public class DeepseekIntegrationApplication {
